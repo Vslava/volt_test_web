869849da99b05c7075bb7adcb6c99b587f3690f9
Viacheslav Voytovich <voytovich.slava@gmail.com>
2017-02-22 04:42:03 +0700

    password validation can be applied only on create

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,14 @@
 class User < ApplicationRecord
   has_secure_password
 
+  mount_uploader :avatar, AvatarUploader
+
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
 
-  validates :nickname, :email, :password, presence: true
+  validates :nickname, :email, presence: true
   validates :nickname, length: { maximum: 100 }
-  validates :password, length: { maximum: 100 }
+  validates :password, presence: true, on: :create
+  validates :password, length: { maximum: 100 }, on: :create
   validates :email, format: /\A[^@\s]+@[^@\s]+\z/
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,9 +4,9 @@ RSpec.describe User, type: :model do
   describe 'validations' do
     it { is_expected.to validate_presence_of :nickname }
     it { is_expected.to validate_presence_of :email }
-    it { is_expected.to validate_presence_of :password }
+    it { is_expected.to validate_presence_of(:password).on(:create) }
     it { is_expected.to validate_length_of(:nickname).is_at_most(100) }
-    it { is_expected.to validate_length_of(:password).is_at_most(100) }
+    it { is_expected.to validate_length_of(:password).is_at_most(100).on(:create) }
     it { is_expected.to allow_value('some@email').for(:email) }
     it { is_expected.not_to allow_value('@').for(:email) }
     it { is_expected.not_to allow_value('@email').for(:email) }
