f1d7bc55ac6f0ef4addd628e1228639d439642f0
Viacheslav Voytovich <voytovich.slava@gmail.com>
2017-02-25 02:01:48 +0700

    Fix ReportMailer spec

--- spec/mailers/report_mailer_spec.rb
+++ spec/mailers/report_mailer_spec.rb
@@ -1,5 +1,5 @@
 require "rails_helper"
 
-RSpec.describe ReportMailerMailer, type: :mailer do
+RSpec.describe ReportMailer, type: :mailer do
   pending "add some examples to (or delete) #{__FILE__}"
 end
