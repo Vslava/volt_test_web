e9f35a053498be0d16607dbdf57fb0a3f5e48ca0
Viacheslav Voytovich <voytovich.slava@gmail.com>
2017-03-10 13:04:20 +0700

    Avatars Controller spec

--- spec/controllers/avatars_controller_spec.rb
+++ spec/controllers/avatars_controller_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe AvatarsController, type: :controller do
+  render_views
 
+  before do
+    user_sign_in
+  end
+
+  describe 'GET #new' do
+    subject { get :new }
+
+    it 'behaves like new' do
+      is_expected.to have_http_status(:ok)
+      is_expected.to render_template('avatars/new')
+    end
+  end
+
+  describe 'POST #create' do
+    pending "add some examples to #{__FILE__}"
+  end
 end
